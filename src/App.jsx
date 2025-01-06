36e1bc22d84d1bc27fddf4401e4d60923bf82fcf
nuhmansha <nuhmanibnunazar@gmail.com>
2025-01-06 13:55:39 +0530

    files added

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+import Option from './components/Option'
+import QuestionNumber from './components/QuestionNumber'
+import QuizApp from './Pages/QuizApp'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+     
+     {/* <Option/>
+     <QuestionNumber/> */}
+     <QuizApp/>
+    </>
+  )
+}
+
+export default App
